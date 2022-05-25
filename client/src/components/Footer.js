134947da08a9ca398c174ac61125aed891bee2a9
igips <igor.szepietowski@gmail.com>
2022-05-25 16:58:35 +0200

    Create module

---
+++ client/src/components/Footer.js
@@ -0,0 +1,16 @@
+import "../styles/Footer.css";
+
+function Footer() {
+
+
+
+    return (
+        <footer>
+            <div id="footer-main-div">
+
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
