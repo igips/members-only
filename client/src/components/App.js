cc75e8f45285a3cfc8823c4d45e71f7c8ded0e8c
igips <igor.szepietowski@gmail.com>
2022-05-30 21:11:36 +0200

    Add browserrouter

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../styles/App.css";
 import Nav from "./Nav";
 import Footer from "./Footer";
+import Home from "./Home";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
 
 function App() {
 	// const [a, setA] = useState(null);
@@ -14,13 +18,17 @@ function App() {
 	// 		});
 	// }, []);
 
-	
-
 	return (
 		<div className="App">
-			<Nav />
-
-			<Footer />
+			<BrowserRouter>
+				<Nav />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/signIn" element={<SignIn />} />
+					<Route path="/signUp" element={<SignUp />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
 		</div>
 	);
 }
