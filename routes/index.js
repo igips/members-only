c4a17d482dd0e5a438e0b980b9c4c475fc067bb9
igips <igor.szepietowski@gmail.com>
2022-05-30 21:13:01 +0200

    Test

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const { body,validationResult } = require('express-validator');
 
 const userController = require("../controllers/userController");
 const messageController = require("../controllers/messageController");
 
-// router.get('/ha', (req, res, next) => {
-//     res.json({ message: "Hello from server!" });
+router.post('/signUp', (req, res, next) => {
+    // res.json({ message: "Hello from server!" });
+    console.log(req.body.username);
+    // res.json();
+    
 
-// });
+});
 
 router.get("/*", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
