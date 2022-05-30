5d6ef744419b89c279084ac4d981f613d5d05a17
igips <igor.szepietowski@gmail.com>
2022-05-30 21:11:52 +0200

    .

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,10 +4,16 @@ function Footer() {
 	return (
 		<footer>
 			<div id="footer-main-div">
-                <span>igips</span>
-				<a href="https://github.com/igips" target="_blank" rel="noreferrer">
-                    <i className="fa-brands fa-github"></i>
-				</a>
+				{/* <div className="fot-user">
+					<i className="fa-solid fa-user"></i>
+					<span>aaaaUser1</span>
+				</div> */}
+				<div className="fot-git">
+					<span>igips</span>
+					<a href="https://github.com/igips" target="_blank" rel="noreferrer">
+						<i className="fa-brands fa-github"></i>
+					</a>
+				</div>
 			</div>
 		</footer>
 	);
