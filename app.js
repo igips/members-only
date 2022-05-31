4c09fc7c70330e72a2d0d5880a716cff2ca8eac5
igips <igor.szepietowski@gmail.com>
2022-05-31 22:32:46 +0200

    Add passport

--- app.js
+++ app.js
@@ -1,31 +1,71 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const mongoose = require('mongoose');
-require('dotenv').config()
-
+const mongoose = require("mongoose");
+const User = require("./models/user.js");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
 const indexRouter = require("./routes/index");
+require("dotenv").config();
 
 const app = express();
 
 const mongoDB = process.env.db;
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.set("view engine", "jade");
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		credentials: true,
+	})
+);
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+passport.use(
+	new LocalStrategy((username, password, done) => {
+		User.findOne({ username: username }, (err, user) => {
+			if (err) {
+				return done(err);
+			}
+			if (!user) {
+				return done(null, false, { message: "Incorrect username!" });
+			}
 
+			bcrypt.compare(password, user.password, (err, res) => {
+				if (res) {
+					return done(null, user);
+				} else {
+					return done(null, false, { message: "Incorrect password!" });
+				}
+			});
+		});
+	})
+);
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
 
+passport.deserializeUser((id, done) => {
+	User.findById(id, (err, user) => {
+		done(err, user);
+	});
+});
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.resolve(__dirname, './client/build')));
-app.use(express.static(path.resolve(__dirname, './client/public')));
-
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/public")));
 
 app.use("/", indexRouter);
 
