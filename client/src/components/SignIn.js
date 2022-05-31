970abd1f9f4d94353063fe7d4e2fa57956198855
igips <igor.szepietowski@gmail.com>
2022-05-31 22:35:06 +0200

    Create components

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,9 +1,92 @@
 import "../styles/SignUpIn.css";
+import spy from "../img/avatars/spy.svg";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SignIn() {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const navigate = useNavigate();
+
+	function handleUsernameChange(e) {
+		setUsername(e.target.value);
+	}
+
+	function handlePasswordChange(e) {
+		setPassword(e.target.value);
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		const usernameInput = document.getElementById("signIn-username-input");
+		const passInput = document.getElementById("passSignIn");
+		const submitButton = document.getElementById("signIn-submit-btn");
+		const btnSpinner = document.getElementById("signIn-btn-spinner");
+		const btnSpan = document.getElementById("signIn-btn-span");
+
+		btnSpan.style.display = "none";
+		btnSpinner.style.display = "block";
+
+		axios
+			.post("/signIn", { username: username, password: password })
+			.then((res) => {
+				btnSpinner.style.display = "none";
+				btnSpan.style.display = "block";
+
+				if (res.data.message === "Incorrect username!") {
+					usernameInput.setCustomValidity("Incorrect username!");
+					submitButton.click();
+				} else if (res.data.message === "Incorrect password!") {
+					passInput.setCustomValidity("Incorrect password!");
+					submitButton.click();
+				} else {
+					navigate("/");
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
+
 	return (
 		<main>
-			<div className="singUpIn-main-container"></div>
+			<div className="singUpIn-main-container">
+				<div className="signUpIn-inner-container">
+					<p className="signUp-In-span">Sign In!</p>
+					<img src={spy} alt="" />
+					<form method="POST" onSubmit={(e) => handleSubmit(e)}>
+						<input
+							id="signIn-username-input"
+							type="text"
+							maxLength="15"
+							minLength="3"
+							placeholder="Username"
+							onChange={(e) => handleUsernameChange(e)}
+							value={username}
+							required
+							onInput={() => document.getElementById("signIn-username-input").setCustomValidity("")}
+						/>
+						<input
+							id="passSignIn"
+							type="password"
+							minLength="6"
+							placeholder="Password"
+							onChange={(e) => handlePasswordChange(e)}
+							value={password}
+                            onInput={() => document.getElementById("passSignIn").setCustomValidity("")}
+							required
+						/>
+						<button id="signIn-submit-btn" type="submit" className="signUp-In-btn">
+							<div id="signIn-btn-spinner">
+								<i className="fa-solid fa-circle-notch fa-spin"></i>
+							</div>
+							<span id="signIn-btn-span">Sign In</span>
+						</button>
+					</form>
+				</div>
+			</div>
 		</main>
 	);
 }
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -2,30 +2,56 @@ import "../styles/SignUpIn.css";
 import spy from "../img/avatars/spy.svg";
 import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SignUp() {
 	const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [rPassword, setRpassword] = useState("");
+	const [password, setPassword] = useState("");
+	const [rPassword, setRpassword] = useState("");
+	const navigate = useNavigate();
 
 	function handleUsernameChange(e) {
 		setUsername(e.target.value);
 	}
 
-    function handlePasswordChange(e) {
+	function handlePasswordChange(e) {
 		setPassword(e.target.value);
 	}
 
-    function handleRpasswordChange(e) {
+	function handleRpasswordChange(e) {
 		setRpassword(e.target.value);
 	}
 
 	function handleSubmit(e) {
 		e.preventDefault();
-		axios.post("/signUp", { username: username });
+
+		const usernameInput = document.getElementById("signUp-username-input");
+		const submitButton = document.getElementById("signUp-submit-btn");
+		const btnSpinner = document.getElementById("signUp-btn-spinner");
+		const btnSpan = document.getElementById("signUp-btn-span");
+
+		btnSpan.style.display = "none";
+		btnSpinner.style.display = "block";
+
+		axios
+			.post("/signUp", { username: username, password: password })
+			.then((res) => {
+				btnSpinner.style.display = "none";
+				btnSpan.style.display = "block";
+
+				if (res.data === "User already exists!") {
+					usernameInput.setCustomValidity("Username already taken!");
+					submitButton.click();
+				} else if (res.data === "success") {
+					navigate("/");
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+			});
 	}
 
-    function checkIfPasswordMatch() {
+	function checkIfPasswordMatch() {
 		const passwordInput = document.getElementById("pass2");
 		const passwordInput2 = document.getElementById("pass1");
 
@@ -44,37 +70,41 @@ function SignUp() {
 					<img src={spy} alt="" />
 					<form method="POST" onSubmit={(e) => handleSubmit(e)}>
 						<input
-                            id="signUp-username-input"
+							id="signUp-username-input"
 							type="text"
 							maxLength="15"
+							minLength="3"
 							placeholder="Username"
 							onChange={(e) => handleUsernameChange(e)}
 							value={username}
 							required
-                            onInput={() => document.getElementById("signUp-username-input").setCustomValidity("")}
+							onInput={() => document.getElementById("signUp-username-input").setCustomValidity("")}
 						/>
 						<input
-                            id="pass1"
+							id="pass1"
 							type="password"
-                            minLength="6"
+							minLength="6"
 							placeholder="Password"
 							onChange={(e) => handlePasswordChange(e)}
 							value={password}
-                            onInput={() => checkIfPasswordMatch()}
+							onInput={() => checkIfPasswordMatch()}
 							required
 						/>
-                        <input
-                            id="pass2"
+						<input
+							id="pass2"
 							type="password"
-                            minLength="6"
+							minLength="6"
 							placeholder="Repeat password"
 							onChange={(e) => handleRpasswordChange(e)}
 							value={rPassword}
-                            onInput={() => checkIfPasswordMatch()}
+							onInput={() => checkIfPasswordMatch()}
 							required
 						/>
-						<button type="submit" className="signUp-In-btn">
-							Sign Up
+						<button id="signUp-submit-btn" type="submit" className="signUp-In-btn">
+							<div id="signUp-btn-spinner">
+								<i className="fa-solid fa-circle-notch fa-spin"></i>
+							</div>
+							<span id="signUp-btn-span">Sign Up</span>
 						</button>
 					</form>
 				</div>
