f939cd3f7dd7433f0fd85524342d3e5e6670ee11
igips <igor.szepietowski@gmail.com>
2022-05-31 22:36:03 +0200

    Add routes

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { body,validationResult } = require('express-validator');
 
 const userController = require("../controllers/userController");
 const messageController = require("../controllers/messageController");
 
-router.post('/signUp', (req, res, next) => {
-    // res.json({ message: "Hello from server!" });
-    console.log(req.body.username);
-    // res.json();
-    
+router.post("/signUp", userController.signUp);
 
-});
+router.post("/signIn", userController.signIn);
+
+router.get("/checkUser", userController.checkUser);
+
+router.get("/signOut", userController.signOut);
 
 router.get("/*", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
