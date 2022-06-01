947135b8a6a0e587e7d901e916e6564cced97289
igips <igor.szepietowski@gmail.com>
2022-06-01 22:19:08 +0200

    Refactor

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -3,12 +3,16 @@ import spy from "../img/avatars/spy.svg";
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setUser } from "../features/user/userSlice";
 
 function SignUp() {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	const [rPassword, setRpassword] = useState("");
 	const navigate = useNavigate();
+	const user = useSelector((state) => state.user.value);
+	const dispatch = useDispatch();
 
 	function handleUsernameChange(e) {
 		setUsername(e.target.value);
@@ -42,7 +46,8 @@ function SignUp() {
 				if (res.data === "User already exists!") {
 					usernameInput.setCustomValidity("Username already taken!");
 					submitButton.click();
-				} else if (res.data === "success") {
+				} else {
+					dispatch(setUser(res.data));
 					navigate("/");
 				}
 			})
@@ -62,55 +67,62 @@ function SignUp() {
 		}
 	}
 
-	return (
-		<main>
-			<div className="singUpIn-main-container">
-				<div className="signUpIn-inner-container">
-					<p className="signUp-In-span">Sign Up!</p>
-					<img src={spy} alt="" />
-					<form method="POST" onSubmit={(e) => handleSubmit(e)}>
-						<input
-							id="signUp-username-input"
-							type="text"
-							maxLength="15"
-							minLength="3"
-							placeholder="Username"
-							onChange={(e) => handleUsernameChange(e)}
-							value={username}
-							required
-							onInput={() => document.getElementById("signUp-username-input").setCustomValidity("")}
-						/>
-						<input
-							id="pass1"
-							type="password"
-							minLength="6"
-							placeholder="Password"
-							onChange={(e) => handlePasswordChange(e)}
-							value={password}
-							onInput={() => checkIfPasswordMatch()}
-							required
-						/>
-						<input
-							id="pass2"
-							type="password"
-							minLength="6"
-							placeholder="Repeat password"
-							onChange={(e) => handleRpasswordChange(e)}
-							value={rPassword}
-							onInput={() => checkIfPasswordMatch()}
-							required
-						/>
-						<button id="signUp-submit-btn" type="submit" className="signUp-In-btn">
-							<div id="signUp-btn-spinner">
-								<i className="fa-solid fa-circle-notch fa-spin"></i>
-							</div>
-							<span id="signUp-btn-span">Sign Up</span>
-						</button>
-					</form>
+	function content() {
+		if (typeof user === "string") {
+			return (
+				<div className="singUpIn-main-container">
+					<div className="signUpIn-inner-container">
+						<p className="signUp-In-span">Sign Up!</p>
+						<img src={spy} alt="" />
+						<form method="POST" onSubmit={(e) => handleSubmit(e)}>
+							<input
+								id="signUp-username-input"
+								type="text"
+								maxLength="15"
+								minLength="3"
+								placeholder="Username"
+								onChange={(e) => handleUsernameChange(e)}
+								value={username}
+								required
+								onInput={() => document.getElementById("signUp-username-input").setCustomValidity("")}
+								autoComplete="off"
+							/>
+							<input
+								id="pass1"
+								type="password"
+								minLength="6"
+								placeholder="Password"
+								onChange={(e) => handlePasswordChange(e)}
+								value={password}
+								onInput={() => checkIfPasswordMatch()}
+								required
+							/>
+							<input
+								id="pass2"
+								type="password"
+								minLength="6"
+								placeholder="Repeat password"
+								onChange={(e) => handleRpasswordChange(e)}
+								value={rPassword}
+								onInput={() => checkIfPasswordMatch()}
+								required
+							/>
+							<button id="signUp-submit-btn" type="submit" className="signUp-In-btn">
+								<div id="signUp-btn-spinner">
+									<i className="fa-solid fa-circle-notch fa-spin"></i>
+								</div>
+								<span id="signUp-btn-span">Sign Up</span>
+							</button>
+						</form>
+					</div>
 				</div>
-			</div>
-		</main>
-	);
+			);
+		} else {
+			return (<></>)
+		}
+	}
+
+	return <main>{content()}</main>;
 }
 
 export default SignUp;
