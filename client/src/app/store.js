2613afb9738b4faee6ef9bacddf24f60acdeed87
igips <igor.szepietowski@gmail.com>
2022-06-02 18:19:11 +0200

    Add message reducer

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
+import messagesReducer from "../features/user/messagesSlice";
+
 
 export default configureStore({
 	reducer: {
 		user: userReducer,
+		messages: messagesReducer,
 	},
 });
