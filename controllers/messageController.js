f5594eeaf81d8cec5d77411d02770681414b659f
igips <igor.szepietowski@gmail.com>
2022-06-02 18:22:21 +0200

    Write message functions

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,7 +1,65 @@
 const Message = require("../models/message");
 const { body, validationResult } = require("express-validator");
 
+exports.message = [
+	body("title").trim().escape(),
+	body("message").trim().escape(),
+	body("user").escape(),
+	(req, res, next) => {
+		const errors = validationResult(req);
 
-exports.example = (req, res) => {
-	res.send("HAHA");
+		if (!errors.isEmpty()) {
+			return next(errors);
+		} else {
+			const message = new Message({
+				title: req.body.title,
+				message: req.body.message,
+				user: req.body.user,
+				avatar: req.body.avatar,
+				timeStamp: Date.now(),
+			});
+
+			message.save((err) => {
+				if (err) {
+					return next(err);
+				}
+
+				res.json(message);
+			});
+		}
+	},
+];
+
+exports.getMessages = (req, res, next) => {
+	Message.find({})
+		.populate("user")
+		.exec((err, messages) => {
+			if (err) {
+				return next(err);
+			}
+
+			messages.reverse();
+
+			res.json(messages);
+		});
+};
+
+exports.deleteMessage = (req, res, next) => {
+	Message.findByIdAndRemove(req.body.messageId, (err) => {
+		if (err) {
+			return next(err);
+		}
+
+		Message.find({})
+			.populate("user")
+			.exec((err, messages) => {
+				if (err) {
+					return next(err);
+				}
+
+				messages.reverse();
+
+				res.json(messages);
+			});
+	});
 };
